1b3eb3a84351d4c031398a8ba8fd244c51fe318e
utk61198 <utk61198@gmail.com>
2022-04-25 16:50:48 +0530

    intersection of arrays

---
+++ studyplan/dsa/intersection_of_two_arrays.py
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d={}
+        a=[]
+        for item in nums1:
+            if item in d.keys():
+                d[item]=d[item]+1
+            else:
+                d[item]=1
+        for item in nums2:
+            if item in d.keys() and d[item]>0:
+                d[item]=d[item]-1
+                a.append(item)
+        return a
