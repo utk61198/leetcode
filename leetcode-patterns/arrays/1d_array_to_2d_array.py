0b447e4b52bc91ce948c5fd22a5480d3a729cb87
utk61198 <utk61198@gmail.com>
2022-05-27 22:46:42 +0530

    1d array to 2d array

---
+++ leetcode-patterns/arrays/1d_array_to_2d_array.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/convert-1d-array-into-2d-array/
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if len(original) != m*n:
+            return []
+        else:
+            ans=[]
+            for i in range(0,len(original),n):
+                ans.append(original[i:i+n])
+        return ans
+            
+        
