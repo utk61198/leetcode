188a76363b17d1860b713f72a58d0ac698b62189
utk61198 <utk61198@gmail.com>
2022-06-04 10:28:15 +0530

    populating next right pointers in each node

---
+++ leetcode-patterns/bfs/next_right_pointers_node.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return root
+        else:
+            q=[root]
+            while len(q)!=0:
+                l=len(q)
+                for i in range(l):
+                    temp=q.pop(0)
+                    if i == l-1:
+                        temp.next = None
+                    else:
+                        temp.next = q[0]
+                    if temp.left:
+                        q.append(temp.left)
+                    if temp.right:
+                        q.append(temp.right)
+            return root
+                    
+        
