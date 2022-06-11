7bc67163478f5e0056ae726d43c5a125f5c8217c
utk61198 <utk61198@gmail.com>
2022-06-12 03:12:37 +0530

    course schedule 2

--- leetcode-patterns/bfs/level_ord_traversal.py
+++ leetcode-patterns/bfs/level_ord_traversal.py
@@ -6,6 +6,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root == None:
